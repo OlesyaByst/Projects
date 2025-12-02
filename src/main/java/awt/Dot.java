be80a7241c7a9f3bbfeb50890fe795177b1824b2
Olesya Bystrushkina <testermpinvest@gmail.com>
2025-12-02 19:28:20 +0300

    Практика ООП. Пакеты #4. Простые имена. Повторный коммит

---
+++ src/main/java/awt/Dot.java
@@ -0,0 +1,15 @@
+package awt;
+
+public class Dot {
+    public int x;
+    public int y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+}
---
+++ src/main/java/ru/courses/main/Practic.java
@@ -0,0 +1,14 @@
+package ru.courses.main;
+
+public class Practic {
+    public static void main(String[] args) {
+        System.out.println("Проверка");
+    }
+
+    public String str1,str2;
+    public Practic(String str1,String str2){
+        this.str1=str1;
+        this.str2=str2;
+    }
+
+}
